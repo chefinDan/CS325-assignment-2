ac96cc59b17e5c1dcb88c844797f1b79a543d6e6
chefinDan <greendan@oregonstate.edu>
2019-02-18 16:05:25 -0800

    added comments and example usage for functions in helper.py

--- helpers.py
+++ helpers.py
@@ -1,56 +1,83 @@
 
 
-# Function to return the minimum value of three numbers
-def min(num1, num2, num3):
-
-    if num1 <= num2 and num1 <= num3:
-        return num1
-    elif num2 <= num1 and num2 <= num3:
-        return num2
-    else:
-        return num3
+# *********************************************************
+# Example usage of the functions can be found at the bottom
+# of the page
+# *********************************************************
 
 
-# def seqFileToList()
-# Takes a string filename as argument, returns a list of dicts
-# Each element of the list has the following format:
+#####################################################
+# function seqFileToList()
+# Takes a string filename as argument, opens the file and
+# returns a list of dicts. Each element of the list has the
+# following format:
 #
 # List[0] = {
 #    'seqA': AGTCGGTACGCT...
 #    'seqB': TCAGGTTAACAC...
 # }
 
+
 def seqFileToList(filename):
+    # list structure to store gene sequence data of the entire file
     listOfPairs = list()
 
     with open(filename, 'r') as file:
-        for rawline in file:
-            tmp = list()
+        for rawline in file:  # get the next unformatted line and put into rawline
+            tmp = list()  # list structure for the actual sequence data
 
+            # Split rawline at the comma and loop through the resulting two strings
+            # strip each string of newline chars and add to the tmp list
             for l in rawline.split(','):
                 tmp.append(l.strip('\n'))
 
+            # append a dict object to main gene sequence list, assigning each sequence
+            # the name seqA or seqB
             listOfPairs.append({'seqA': tmp[0], 'seqB': tmp[1]})
 
     return listOfPairs
 
 
-# function to put imp2cost.txt into a 2-d list
+# ######################################################
+# function to put imp2cost.txt into a 2-d list.
+# Takes a string filename as argument, opens the corresponding file,
+# formats the costfile information and returns a 2-d list.
+# The costList returned is not be accessed directly, but passed to other
+# functions where needed.
 def costFileToList(filename):
-    rawCostList = list()
-    costList = dict()
+    rawCostList = list()  # tmp list object
+    costList = list()  # list object to be returned
 
+    # open file named <filename>, loop through the lines, appending
+    # the formatted line to rawCostList
     with open(filename, 'r') as file:
         for rawline in file:
             line = rawline.strip('\n').split(',')
             rawCostList.append(line)
 
+    # Start at 1, to avoid copying the title row of the cost file.
+    # Append each row from costFile to costList, again starting at 1 to
+    # avoid copying the title column.
     for i in range(1, 6):
-        for j in rawCostList[i]:
-            costList[j] = rawCostList[i][1:]
-            break
+        costList.append(rawCostList[i][1:])
+        # print costList
 
     return costList
+
+
+# ######################################################################
+# Function to get the cost of aligning letters a and b. Uses costList
+# returned by costFileToList().
+#
+def cost(costList, a, b):
+    if a == b:
+        return 0
+    else:
+        c = letterToIdx(a)
+        d = letterToIdx(b)
+        if c is not None and d is not None:
+            return costList[c][d]
+    return None
 
 
 # helper to convert a Gene letter to it's corresponding array index
@@ -66,13 +93,17 @@ def letterToIdx(x):
     return letters.get(x)
 
 
-# Function to get the cost of aligning letters a and b. Uses costList
-# returned by costFileToList().
-def cost(costList, a, b):
-    if a == b:
-        return 0
-    else:
-        c = letterToIdx(b)
-        if c is not None:
-            return costList[a][c]
-    return None
+# ************* Example usage **********************************
+seqfile = 'imp2input.txt'
+seqlist = seqFileToList(seqfile)
+print seqlist
+
+costfile = 'imp2cost.txt'
+costlist = costFileToList(costfile)
+cost1 = cost(costlist, 'A', 'G')
+cost2 = cost(costlist, '-', 'T')
+cost3 = cost(costlist, 'C', 'T')
+
+min = min(cost1, cost2, cost3)
+
+print min
